d3976697edae45fc389dd834b067d7494b2b9eae
Ajita Lamsal <ajitalamsal@Ajitas-MBP.attlocal.net>
2020-07-23 12:41:20 -0500

    branch1

--- src/branch2/java/branch2.java
+++ src/branch2/java/branch2.java
@@ -2,7 +2,7 @@ package branch2.java;
 
 public class branch2 {
     public static void main(String[] args) {
-        System.out.println("This is branch1");
+        System.out.println("This is branch2");
 
 
     }
